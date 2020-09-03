c06ffef973e1fab709c690539fe05f4e43ed6a39
javalsh <javalsh@126.com>
2020-09-03 18:32:23 +0800

    项目结构初始化

---
+++ mall-member/src/main/java/com/lsh/mall/member/MallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.lsh.mall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallMemberApplication.class, args);
+	}
+
+}
---
+++ mall-product/src/test/java/com/lsh/mall/product/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsh.mall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
