7e4f6d34a667e158438a3ae16b04a3cc66d4a42f
javalsh <javalsh@126.com>
2020-09-22 22:20:29 +0800

    配置nacos注册中心

--- mall-member/src/main/java/com/lsh/mall/member/MallMemberApplication.java
+++ mall-member/src/main/java/com/lsh/mall/member/MallMemberApplication.java
@@ -3,8 +3,10 @@ package com.lsh.mall.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.lsh.mall.member.dao")
+@EnableFeignClients
 @SpringBootApplication
 public class MallMemberApplication {
 
